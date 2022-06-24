353def2e280a05166d1b81a04f68ac75fd598022
vadym1202 <vadim.paliushek@gmail.com>
2022-06-24 21:50:15 +0900

    update hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomiclabs/hardhat-waffle");
 
+require('dotenv').config();
+
 module.exports = {
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {
+    },
+    matic: {
+      url: "https://polygon-rpc.com/",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  // etherscan: {
+  //   apiKey: process.env.POLYGONSCAN_API_KEY
+  // },
   solidity: "0.8.4",
   paths: {
     artifacts: "./src/backend/artifacts",
